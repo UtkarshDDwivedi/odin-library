14df40bd9f183b997c1c58a3d0acea3ebbbbd96f
Utkarsh <utkarshdhar05@gmail.com>
2025-06-04 20:17:29 +0530

    Modal Box Style

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function Book(title, author, pages, progress) {
     this.progress = progress;
     this.id = crypto.randomUUID();
 }
+
+let dialog = document.querySelector("dialog");
+let showModalBtn = document.querySelector("header button");
+let closeModalBtn = document.querySelector("#cancel");
+let addBookBtn = document.querySelector("#add-book");
+
+showModalBtn.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+closeModalBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+})
